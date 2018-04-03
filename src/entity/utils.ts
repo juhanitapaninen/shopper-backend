d510974b2c613a414d21f37219cf5d8d5dbce77d
Juhani Tapaninen <juhani.tapaninen@gmail.com>
2018-04-03 22:26:26 +0300

    update dependencies.

--- src/entity/utils.ts
+++ src/entity/utils.ts
@@ -2,7 +2,7 @@ import * as S from "sanctuary";
 import { ShoppingListType, ItemType } from "../entity";
 
 export const findOrCreate = async (name: string, entityType: any) => {
-  const maybeType: S.Maybe<ShoppingListType | ItemType> = S.head(await entityType.find({where: {name}}));
+  const maybeType: any = S.head(await entityType.find({where: {name}}));
   const type = S.fromMaybe(new entityType())(maybeType);
   if (maybeType === S.Nothing) {
     type.name = name;
